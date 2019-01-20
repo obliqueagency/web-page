a237b5610b3d7e907ffa3da065a30fbd49ac6a58
ines.kulenovic <ines.kulenovic>
2019-01-20 21:33:57 +0100

    Updates

---
+++ precache-manifest.10cb3b8b51def1adf47d37f6c178bcce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad92e8dabebabf5808e5",
+    "url": "/web-page/static/js/runtime~main.ad92e8da.js"
+  },
+  {
+    "revision": "dd15d6730fd4eb373528",
+    "url": "/web-page/static/js/main.dd15d673.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/web-page/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dd15d6730fd4eb373528",
+    "url": "/web-page/static/css/main.270cfb7b.chunk.css"
+  },
+  {
+    "revision": "6a700f5d6329d74cc56abd1ef472fe89",
+    "url": "/web-page/index.html"
+  }
+];
